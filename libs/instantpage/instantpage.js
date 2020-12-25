8bf629fc9f5984824f5aceaf8060f4843ec86810
shuai.hu <shuai.hu@fksaas.com>
2020-12-25 15:09:09 +0800

    'update'

--- libs/instantpage/instantpage.js
+++ libs/instantpage/instantpage.js
@@ -1,4 +1,4 @@
-/*! instant.page v2.0.0 - (C) 2019 Alexandre Dieulot - https://instant.page/license */
+/*! instant.page v2.0.0 - (C) 2020 Alexandre Dieulot - https://instant.page/license */
 
 let urlToPreload
 let mouseoverTimer
